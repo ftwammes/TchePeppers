e21d77f41499812d5575ffb143c335c59ddf4476
ftwammes <wammesfernanda@gmail.com>
2025-05-28 21:33:52 -0300

    Ajustes nos arbustos e nas esculturas

--- Scripts/pipe.gd
+++ Scripts/pipe.gd
@@ -2,21 +2,22 @@ extends StaticBody2D
 
 class_name Pipe
 
-const TOP_PIPE_HEIGHT = 16
+const TOP_PIPE_HEIGHT = 25.5
 
 @export var height = 32
 @export var is_traversable = false
 
 @onready var collision_shape_2d = $CollisionShape2D
-@onready var pipe_body_sprite = $PipeBodySprite
+@onready var body_sprite = $SpriteBase
+@onready var sprite = $Sprite
 
 func _ready():
-	var region_rect = Rect2(pipe_body_sprite.region_rect)
-	region_rect.size = Vector2(32, height - TOP_PIPE_HEIGHT)
-	pipe_body_sprite.region_rect = region_rect
-	pipe_body_sprite.position = Vector2(0, height / 2)
+	var region_rect = Rect2(body_sprite.region_rect)
+	region_rect.size = Vector2(32, sprite.position.y)
+	body_sprite.region_rect = region_rect
+	body_sprite.position = Vector2(0, (sprite.position.y  - TOP_PIPE_HEIGHT) / -2)
 	
 	var shape = RectangleShape2D.new()
-	shape.size = Vector2(32, height)
+	shape.size = Vector2(7.5, height)
 	collision_shape_2d.shape = shape
-	collision_shape_2d.position = Vector2(0, height /2 - TOP_PIPE_HEIGHT / 2)
+	collision_shape_2d.position = Vector2(0, (height - TOP_PIPE_HEIGHT) / 2)
